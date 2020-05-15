70db252c38cd8f4e6dd32671d06fec1ffd81fcfb
Eurelian <44678944+Eurelian@users.noreply.github.com>
2020-05-15 17:06:25 +0200

    First one

--- src/App.js
+++ src/App.js
@@ -1,31 +1,8 @@
 import React, { useEffect } from "react";
 import "./App.css";
-import Featured from "./components/featured";
-import { ThemeProvider } from "@material-ui/styles";
-import theme from "./components/UI/theme";
 
 function App() {
-	useEffect(() => {
-		const contentful = require("contentful");
-		const client = contentful.createClient({
-			space: "judmcnqfm2ry",
-
-			accessToken: "Cpz2_8v_83-s3bHAYAP1a8mUV1fkFUuT7MEK3UsW-Wg",
-		});
-
-		client
-			.getEntries({ content_type: "shirt" })
-			.then((entry) => console.log(entry))
-			.catch((err) => console.log(err));
-	}, []);
-
-	return (
-		<div className='App'>
-			<ThemeProvider theme={theme}>
-				<Featured />
-			</ThemeProvider>
-		</div>
-	);
+	return <div className='App'></div>;
 }
 
 export default App;
---
+++ src/components/services/contentful.js
@@ -0,0 +1,13 @@
+// useEffect(() => {
+// 	const contentful = require("contentful");
+// 	const client = contentful.createClient({
+// 		space: "judmcnqfm2ry",
+
+// 		accessToken: "Cpz2_8v_83-s3bHAYAP1a8mUV1fkFUuT7MEK3UsW-Wg",
+// 	});
+
+// 	client
+// 		.getEntries({ content_type: "shirt" })
+// 		.then((entry) => console.log(entry))
+// 		.catch((err) => console.log(err));
+// }, []);
