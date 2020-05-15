91959a02dc6234340391af86b7556a87b057e8bb
JosyPraveena <josypraveena@gmail.com>
2020-05-15 16:33:39 +0200

    updated App.css

--- src/App.js
+++ src/App.js
@@ -4,9 +4,27 @@ import Footer from "./components/Footer"
 import "./App.css"
 
 function App() {
+  
+  // useEffect( () => { 
+
+  //   const contentful = require("contentful");
+  //   const client = contentful.createClient({
+    
+  //     space: "judmcnqfm2ry",
+    
+  //     accessToken: "Cpz2_8v_83-s3bHAYAP1a8mUV1fkFUuT7MEK3UsW-Wg"
+  //   });
+    
+  //   client
+  //     .getEntries( { content_type: "comicBooks" } )
+  //     .then(entry => console.log(entry))
+  //     .catch(err => console.log(err));
+
+  // }, [] )
+
   return (
     <div className="App">
-      <div> <Slider/></div>
+      <div> <Slider /></div>
       <div><Footer/></div>
     </div>
   );
