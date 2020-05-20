33d6bb546f54bfaa2cba7dd4187595b138383707
Eurelian <44678944+Eurelian@users.noreply.github.com>
2020-05-20 09:37:23 +0200

    20-May

--- src/components/CategoryPage.js
+++ src/components/CategoryPage.js
@@ -1,21 +1,41 @@
 import React, { Component } from "react";
 import Container from "@material-ui/core/Container";
 import Grid from "@material-ui/core/Grid";
+import ProductCard from "./productCard";
 
 const CategoryPage = ({ data }) => {
 	const { items } = data;
 	console.log(items);
 
-	const itemList = items.map((item) => (
-		<div>
-			<div key={item.fields.bookId}>{item.fields.bookTitle}</div>
-			<img src={item.fields.bookImage.fields.file.url} />
-		</div>
-	));
+	// const itemList = items.map((item) => (
+	// 	<div>
+	// 		<div key={item.fields.bookId}>{item.fields.bookTitle}</div>
+	// 		<img src={item.fields.bookImage.fields.file.url} />
+	// 	</div>
+	// ));
 
 	return (
 		<Container>
-			<Grid style={{ maxWidth: "1240px", marginTop: "100px" }}>{itemList}</Grid>
+			<Grid container style={{ maxWidth: "1240px", marginTop: "100px" }}>
+				<Grid item xs={12} sm={6} lg={4}>
+					<ProductCard />
+				</Grid>
+				<Grid item xs={12} sm={6} lg={4}>
+					<ProductCard />
+				</Grid>
+				<Grid item xs={12} sm={6} lg={4}>
+					<ProductCard />
+				</Grid>
+				<Grid item xs={12} sm={6} lg={4}>
+					<ProductCard />
+				</Grid>
+				<Grid item xs={12} sm={6} lg={4}>
+					<ProductCard />
+				</Grid>
+				<Grid item xs={12} sm={6} lg={4}>
+					<ProductCard />
+				</Grid>
+			</Grid>
 		</Container>
 	);
 };
