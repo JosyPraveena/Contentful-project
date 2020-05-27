87977b66b84fae1c3dc679d9309e4cbd41acbaf6
Eurelian <44678944+Eurelian@users.noreply.github.com>
2020-05-27 15:11:40 +0200

    ok

--- src/App.js
+++ src/App.js
@@ -21,35 +21,36 @@ function App() {
 	const [shirtData, setShirtData] = useState("");
 	const [blogData, setBlogData] = useState("");
 
+	const fetchData = async () => {
+		try {
+			const response = await fetch("http://localhost:3001/shirt/");
+			const shirtData = await response.json();
+			console.log(shirtData);
+		} catch (err) {
+			console.error(err.message);
+		}
+
+		try {
+			const response = await fetch("http://localhost:3001/mug/");
+			const mugData = await response.json();
+			console.log(mugData);
+		} catch (err) {
+			console.error(err.message);
+		}
+
+		try {
+			const response = await fetch("http://localhost:3001/blog/");
+			const blogData = await response.json();
+			console.log(blogData);
+		} catch (err) {
+			console.error(err.message);
+		}
+	};
+
 	useEffect(() => {
 		fetchData();
 	}, []);
 
-	const fetchData = async () => {
-		const contentful = require("contentful");
-		const client = contentful.createClient({
-			space: "judmcnqfm2ry",
-			accessToken: "Cpz2_8v_83-s3bHAYAP1a8mUV1fkFUuT7MEK3UsW-Wg",
-		});
-
-		client
-			.getEntries({ content_type: "comicBooks" })
-			.then((entry) => setData(entry))
-			.catch((err) => console.log(err));
-		client
-			.getEntries({ content_type: "mug" })
-			.then((entry) => setMugData(entry))
-			.catch((err) => console.log(err));
-		client
-			.getEntries({ content_type: "shirt" })
-			.then((entry) => setShirtData(entry))
-			.catch((err) => console.log(err));
-		client
-			.getEntries({ content_type: "blog" })
-			.then((entry) => setBlogData(entry))
-			.catch((err) => console.log(err));
-			
-	};
 	const { items } = blogData;
 
 	return (
@@ -60,48 +61,51 @@ function App() {
 				)}
 			<Navbar />
 			<Switch>
-                <Route
-                    path={"/category/:id/:product"}
-                    render={(props) => (
-                        <Product
-                            data={data}
-                            mugData={mugData}
-                            shirtData={shirtData}
-                            {...props}
-                        />
-                    )}
-                />
-                <Route
-                    path={"/category/:id"}
-                    render={(props) => (
-                        <CategoryPage
-                            data={data}
-                            mugData={mugData}
-                            shirtData={shirtData}
-                            {...props}
-                        />
-                    )}
-                />
-        		<Route
+				<Route
+					path={"/category/:id/:product"}
+					render={(props) => (
+						<Product
+							data={data}
+							mugData={mugData}
+							shirtData={shirtData}
+							{...props}
+						/>
+					)}
+				/>
+				<Route
+					path={"/category/:id"}
+					render={(props) => (
+						<CategoryPage
+							data={data}
+							mugData={mugData}
+							shirtData={shirtData}
+							{...props}
+						/>
+					)}
+				/>
+				<Route
 					path={"/blog/:id"}
 					render={(props) => <BlogDetails blogData={blogData} {...props} />}
 				/>
-				<Route path='/blog/' render={(props) => <BlogMain blogData={blogData} {...props} />}/>
-                <Route exact
-                    path='/'
-                    render={(props) => (
-                        
-						<HomePage  
-                            data={data}
-                            mugData={mugData}
-                            shirtData={shirtData}
-                            {...props}
-                        />
-                    )}
-                />
-				
-                <Route component={PageNotFound} />
-            </Switch>
+				<Route
+					path='/blog/'
+					render={(props) => <BlogMain blogData={blogData} {...props} />}
+				/>
+				<Route
+					exact
+					path='/'
+					render={(props) => (
+						<HomePage
+							data={data}
+							mugData={mugData}
+							shirtData={shirtData}
+							{...props}
+						/>
+					)}
+				/>
+
+				<Route component={PageNotFound} />
+			</Switch>
 		</ThemeProvider>
 	);
 }
